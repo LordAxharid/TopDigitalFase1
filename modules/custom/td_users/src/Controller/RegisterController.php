533b79d9c55201af3f4e36fbbce8609b05e4dafb
Lord Axharid <jj@150porciento.com>
2021-12-23 09:28:11 -0500

    first commit

---
+++ modules/custom/td_users/src/Controller/RegisterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\td_users\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+
+class RegisterController extends ControllerBase{  
+
+ public function registerForm(){
+
+        $entity = \Drupal::entityTypeManager()->getStorage('user')->create(array());
+        $formObject = \Drupal::entityTypeManager()
+        ->getFormObject('user', 'register')
+        ->setEntity($entity);
+        $form = \Drupal::formBuilder()->getForm($formObject);
+
+        return $form;
+          
+    }
+
+  
+
+}
