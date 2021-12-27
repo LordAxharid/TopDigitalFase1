7eadb4a22c908f643fd665a510b3bda81e8d1b8f
LordAxharid <jhonjairomedina11@gmail.com>
2021-12-27 09:12:23 -0500

    adelanto creacion campos

--- web/modules/custom/td_users/src/Migrations/td_users_install.php
+++ web/modules/custom/td_users/src/Migrations/td_users_install.php
@@ -7,6 +7,9 @@ use Drupal\Core\TypedData\DataDefinition;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\co_150_core\Controller\ContentController;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+
 
 use Drupal\td_users\Service\Profile as ProfileService;
 
@@ -17,11 +20,18 @@ use Drupal\profile\Entity\ProfileType;
  * 
  */
 function td_users_install(){
-  // $profile_type = ProfileType::load('user_register_form');
-  // if (!$profile_type) {
-      ProfileService::create();
-  // }
-  // return $profile_type;
+
+  td_users_entity_base_field_info();
+
+}
+
+function td_users_entity_base_field_info(EntityTypeInterface $entity_type) {
+  if ($entity_type->id() === 'user') {
+    $fields['field1'] = BaseFieldDefinition::create('text')
+      ->setLabel(t('New field 1'))
+      ->setTranslatable(TRUE);
+    return $fields;
+  }
 }
 
 /**
