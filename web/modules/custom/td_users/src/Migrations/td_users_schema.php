a7aa6830be7320947f2d70deffa31aad37d9312e
Jhon Sebastian Gomez Estupiñan <jhonsgomz@gmail.com>
2021-12-28 12:56:59 -0500

    add change schema

--- web/modules/custom/td_users/src/Migrations/td_users_schema.php
+++ web/modules/custom/td_users/src/Migrations/td_users_schema.php
@@ -10,29 +10,69 @@ function td_users_uninstall() {
 
 function td_users_schema() {
     $schema['td_users_countries'] =  [
-        'description' => 'Guarda los datos de los usuarios que realicen la trivia',
+        'description' => 'Paises',
         'fields' => [
             'id' => [
-                'type' => 'serial',
-                'not null' => TRUE,
-                'description' => 'primary key: Unique register ID',
+            'type' => 'serial',
+            'unsigned' => TRUE,
+            'not null' => TRUE,
             ],
-            'optional_information' => [
+            'name' => [
                 'type' => 'varchar',
-                'length' => 255,
+                'length' => 100,
+                'null' => TRUE,
             ],
-
+            'prefix' => [
+                'type' => 'varchar',
+                'length' => 4,
+                'null' => TRUE,
+            ],
+            'iso' => [
+                'type' => 'varchar',
+                'length' => 4,
+                'null' => TRUE,
+            ],
             'created_at' => [
                 'mysql_type' => 'timestamp DEFAULT CURRENT_TIMESTAMP',
                 'not null' => TRUE,
             ],
             'updated_at' => [
-            'mysql_type' => 'timestamp DEFAULT CURRENT_TIMESTAMP',
-            'not null' => TRUE,
+                'mysql_type' => 'timestamp DEFAULT CURRENT_TIMESTAMP',
+                'not null' => TRUE,
             ],
         ],
         'primary key' => ['id'],
     ];
 
+    $schema['td_users_cities'] =  [
+        'description' => 'Ciudades',
+        'fields' => [
+            'id' => [
+            'type' => 'serial',
+            'unsigned' => TRUE,
+            'not null' => TRUE,
+            ],
+            'cid' => [
+                'type' => 'int',
+                'size' => 'normal',
+                'null' => TRUE,
+                'description' => 'Countryid',
+            ],
+            'name' => [
+                'type' => 'varchar',
+                'length' => 60,
+                'null' => TRUE,
+            ],
+            'created_at' => [
+                'mysql_type' => 'timestamp DEFAULT CURRENT_TIMESTAMP',
+                'not null' => TRUE,
+            ],
+            'updated_at' => [
+                'mysql_type' => 'timestamp DEFAULT CURRENT_TIMESTAMP',
+                'not null' => TRUE,
+            ],
+        ],
+        'primary key' => ['id'],
+    ];
     return $schema;
 }
